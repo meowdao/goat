1a5e39566365865be59d93e56f5f234be0db1f98
dmydry <dmitriy.gurinenko@gmail.com>
2016-01-18 13:31:51 +0800

    Twitter Search

---
+++ server/configs/config.sample.js
@@ -0,0 +1,124 @@
+"use strict";
+
+export default {
+	test: {
+		mongo: {
+			url: "mongodb://localhost/goat_test",
+			options: {
+				server: {
+					socketOptions: {
+						keepalive: 1
+					}
+				}
+			}
+		},
+		strategies: {
+			local: {
+				usernameField: "email",
+				passwordField: "password"
+			},
+			facebook: {
+				clientID: "",
+				clientSecret: "",
+				callbackURL: "http://localhost:3000/auth/facebook/callback"
+			},
+			google: {
+				clientID: "",
+				clientSecret: "",
+				callbackURL: "http://localhost:3000/auth/google/callback"
+			}
+		},
+		session: {
+			proxy: false,
+			secret: "keyboard_cat",
+			saveUninitialized: true,
+			resave: false,
+			rolling: true,
+			name: "goat.id",
+			cookie: {
+				path: "/",
+				httpOnly: true,
+				secure: false,
+				maxAge: 24 * 60 * 60 * 1000,
+				signed: false
+			}
+		},
+		server: {
+			self: {
+				protocol: "http",
+				hostname: "localhost",
+				port: 3001
+			},
+			mailgun: {
+				api_key: "", // eslint-disable-line camelcase
+				domain: "",
+				from: "G.O.A.T. <no-reply@goat.com>"
+			},
+			twilio: {
+				AccountSID: "",
+				AuthToken: "",
+				from: "+15005550006" // https://www.twilio.com/docs/api/rest/test-credentials
+			}
+		}
+	},
+	development: {
+		mongo: {
+			url: "mongodb://localhost/goat_dev",
+			options: {
+				server: {
+					socketOptions: {
+						keepalive: 1
+					}
+				}
+			}
+		},
+		strategies: {
+			local: {
+				usernameField: "email",
+				passwordField: "password"
+			},
+			facebook: {
+				clientID: "",
+				clientSecret: "",
+				callbackURL: "http://localhost:3000/auth/facebook/callback"
+			},
+			google: {
+				clientID: "",
+				clientSecret: "",
+				callbackURL: "http://localhost:3000/auth/google/callback"
+			}
+		},
+		session: {
+			proxy: false,
+			secret: "keyboard_cat",
+			saveUninitialized: true,
+			resave: false,
+			rolling: true,
+			name: "goat.id",
+			cookie: {
+				path: "/",
+				httpOnly: true,
+				secure: false,
+				maxAge: 24 * 60 * 60 * 1000,
+				signed: false
+			}
+		},
+		server: {
+			self: {
+				protocol: "http",
+				hostname: "localhost",
+				port: 3001
+			},
+			mailgun: {
+				api_key: "", // eslint-disable-line camelcase
+				domain: "",
+				from: "G.O.A.T. <no-reply@goat.com>"
+			},
+			twilio: {
+				AccountSID: "",
+				AuthToken: "",
+				from: ""
+			}
+		}
+	}
+};
