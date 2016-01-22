93ba4f00b72e7a66250ed4b81c9ecbbb14685b8d
dmydry <dmitriy.gurinenko@gmail.com>
2016-01-23 01:20:54 +0800

    webpack-dev-middleware, webpack-hot-middleware

--- server/server.js
+++ server/server.js
@@ -4,17 +4,27 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 process.env.PORT = process.env.PORT || 3000;
 
 import debug from "debug";
-import express from "./configs/express.js";
-
-
-if (process.env.NODE_ENV !== "production") {
-	debug.enable("log:*");
-	debug.enable("controller:*");
-	debug.enable("model:*");
-}
+import app from "./configs/express.js";
+import webpack from "webpack";
 
 const log = debug("log:server");
-const app = express();
+
+debug.enable("log:*");
+debug.enable("controller:*");
+debug.enable("model:*");
+debug.enable("server:*");
+
+if (process.env.NODE_ENV !== "production") {
+
+	const config = require("./configs/webpack");
+	const compiler = webpack(config);
+	const webpackdev = require("./configs/webpack.dev");
+
+	app.use(require('webpack-dev-middleware')(compiler, webpackdev));
+	app.use(require('webpack-hot-middleware')(compiler));
+
+}
+
 
 require("./configs/middleware.js").default(app);
 require("./configs/routes.js").default(app);
@@ -27,3 +37,5 @@ app.listen(process.env.PORT, () => {
 process.on("uncaughtException", (exception) => {
 	log(exception);
 });
+
+
