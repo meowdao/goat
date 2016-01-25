0ee3f2ddd21cbd88672e64855a594b039fda6de5
dmydry <dmitriy.gurinenko@gmail.com>
2016-01-25 12:03:54 +0800

    express.js - export default function

--- server/configs/express.js
+++ server/configs/express.js
@@ -13,25 +13,28 @@ import session from "./session.js";
 import passport from "./passport.js";
 import proxy from "./proxy.js";
 
-const app = express();
+export default function () {
 
-if (process.env.NODE_ENV == "production") {
-	app.use("/build", express.static("./client/build"));
+	let app = express();
+
+	if (process.env.NODE_ENV == "production") {
+		app.use("/build", express.static("./client/build"));
+	}
+
+	app.disable("x-powered-by");
+
+	app.use(logger("dev")); // "default", "short", "tiny", "dev"
+	app.use(cookieParser("keyboardcat"));
+
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+
+	proxy(app);
+	session(app);
+	passport(app);
+
+
+	return app;
 }
-
-app.disable("x-powered-by");
-
-app.use(logger("dev")); // "default", "short", "tiny", "dev"
-app.use(cookieParser("keyboardcat"));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-
-proxy(app);
-session(app);
-passport(app);
-
-
-export default app;
